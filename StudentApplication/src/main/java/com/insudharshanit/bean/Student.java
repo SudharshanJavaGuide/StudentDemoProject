f979b3a32a4dffa92653482619c26ed6a6a79ba2
Sudharshan <sudharshanjavaguide@gmail.com>
2021-11-11 18:00:35 +0530

    DisplayStudetnInfo(),toString() Methods Added

--- StudentApplication/src/main/java/com/insudharshanit/bean/Student.java
+++ StudentApplication/src/main/java/com/insudharshanit/bean/Student.java
@@ -11,8 +11,17 @@ public class Student {
 		this.studentName=studentname;
 		this.studentAddr=studentAddr;
 	}
+	public void displayStudentInfo(Student s) {
+		System.out.println(s);
+	}
 	public static void main(String[] args) {
 		Student s1=new Student(1001,"Sudharshan","Kadapa");
 		Student s2=new Student(1002,"Indra","Chittoor");
+		s1.displayStudentInfo(s1);
+	}
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddr=" + studentAddr
+				+ "]";
 	}
 }
