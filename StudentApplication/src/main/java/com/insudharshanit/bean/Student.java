61ee5b3b803d3cfcdf0ebb62e8dff768768f8ed7
Sudharshan <sudharshanjavaguide@gmail.com>
2021-11-11 17:35:38 +0530

    Constructor Added

--- StudentApplication/src/main/java/com/insudharshanit/bean/Student.java
+++ StudentApplication/src/main/java/com/insudharshanit/bean/Student.java
@@ -5,4 +5,10 @@ public class Student {
 	private int studentId;
 	private String studentName;
 	private String studentAddr;
+	
+	public Student(int studentId,String studentname,String studentAddr) {
+		this.studentId=studentId;
+		this.studentName=studentname;
+		this.studentAddr=studentAddr;
+	}
 }
